bb4e0bf3639ff531e2fe23db7cba3d78fe8d4d5b
chusiping <chusiping@163.com>
2022-07-01 16:30:21 +0800

    2022-7-1 16:29 kong网关sh增加变量

--- kong网关api管理/kong_addService.sh
+++ kong网关api管理/kong_addService.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 # 执行命令： /root/NodeJsApp/kong网关api管理/kong_addService.sh
 
+serviceName="FormMake"
+routeName="FormMake_rt1"
+route_Path="/form"
+consumerName="Jason"
+plugin="basic-auth"
+url="http://121.4.43.207"
+pluginsName="basic-auth"
+consumersUserName="Jason"
+uname="test"
+pwd="123456"
+
+
 
 # Example 3  curl http://redis.qy:8000/form/formake.html
     # 服务入口：name服务名,url服务体
         cmd=`
         curl -s -X POST \
         --url http://127.0.0.1:8001/services/ \
-        --data 'name=FormMake' \
-        --data 'url=http://121.4.43.207'`
+        --data "name=${serviceName}" \
+        --data "url=${url}"`
         echo $cmd 
     # 路由入口：name路由名,hosts访问域名，paths访问路径
-       
         # 创建
         cmd=`
         curl -s -X POST \
-        --url http://127.0.0.1:8001/services/FormMake/routes \
-        --data 'paths=/form' \
-        --data 'strip_path=false' \
-        --data 'name=FormMake_rt1' `
+        --url http://127.0.0.1:8001/services/${serviceName}/routes \
+        --data "paths=/form" \
+        --data "strip_path=false" \
+        --data "name=${routeName}" `
         echo $cmd 
 
         # curl http://redis.qy:8000/top
@@ -27,20 +38,20 @@
 
         # 添加 插件
         cmd=`
-        curl -s -XPOST --data "name=basic-auth" \
-        http://localhost:8001/services/FormMake/plugins  `
+        curl -s -XPOST --data "name=${pluginsName}" \
+        http://127.0.0.1:8001/services/${serviceName}/plugins  `
         echo $cmd 
 
         # 添加 consumer
         cmd=`
-        curl -s -XPOST --data "username=Jason" \
-        http://localhost:8001/consumers/  `
+        curl -s -XPOST --data "username=${consumersUserName}" \
+        http://127.0.0.1:8001/consumers/  `
         echo $cmd 
 
         # 添加 username/password
         cmd=`
-        curl -s -XPOST --data "username=test&password=123456" \
-        http://localhost:8001/consumers/Jason/basic-auth  `
+        curl -s -XPOST --data "username=${uname}&password=${pwd}" \
+        http://127.0.0.1:8001/consumers/${consumersUserName}/${pluginsName}  `
         echo $cmd 
 
 
