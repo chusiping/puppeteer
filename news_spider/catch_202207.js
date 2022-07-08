658c2cc23ca4eaa69c7873df8762e43d84e4edd4
chusiping <chusiping@163.com>
2022-07-08 17:37:19 +0800

    2022-7-8 17:36 edti catch_202207.js 增加日志

--- news_spider/catch_202207.js
+++ news_spider/catch_202207.js
@@ -6,75 +6,89 @@ const sleep = (tim) => new Promise((res, rej) => setTimeout(res, tim));
 const SqliteDB = require('../public/_LibSqlite.js').SqliteDB;
 const file = "new_spider.db";
 const sqliteDB = new SqliteDB(file);
-const IndexA = lib.RowIndex(); //递增
-const IndexB = lib.RowIndex();
+var IndexA = lib.RowIndex(); //递增
+var IndexB = lib.RowIndex();
 const cheerio = require('cheerio')
+var fs = require('fs');
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+log4js.configure({appenders: [{ type:'file',filename: './log/default.log'}]}) //log4js 日志
 
-const urls = [
-    // {
-    //     site:"小众软件", 
-    //     url: ["https://www.appinn.com/category/online-tools/"],
-    //     dv:'#content_box div.post-data div header',  //所有的header集合
-    //     selector : "x.children[0].textContent + \"|||\" + x.children[0].children[0].href + \"|||\" + x.children[1].children[1].children[1].textContent",
-    //     IsShow : true
-    // },
+var urls = [
     {
         site:"donews", 
+        each:".normal-item", // 循环每个titile的 class 或者 id
         url: ["https://www.donews.com/","https://www.donews.com/automobile/index","https://www.donews.com/digital/index"],
-        dv:'div.content',
-        selector : "x.children[2].children[0].textContent + \"|||\" + x.children[2].children[0].href + \"|||\" + x.children[3].children[1].textContent ",
-        IsShow : true
+        regHref:"<span class=\"title\">(\\s|.)*?</span>", //找出<span>...</span>里的href和title
+        regDate:"((.(分钟前|小时前|天前))|(\\d{4}-\\d{1,2}-\\d{1,2}))",
+        IsShow : true,
+        IsCuteTitle:false,
+        IsFixHref : false 
+    },
+    {
+        site:"小众软件", 
+        each:'[class="post-data"]', 
+        url: ["https://www.appinn.com/category/online-tools/"],
+        regHref:'<h2(\\s|.)*?</h2>|<div class="post-title">(\\s|.)*?</div>',
+        regDate:"(.(分钟前|小时前|天前))|(\\d{4}(-|/)\\d{1,2}(-|/)\\d{1,2})|(\\d{1,2}月(\s|.)*\\d{4})",
+        // regDate:"(\\d{1,2}月(\s|.)*\\d{4})",
+        IsShow : true,
+        IsCuteTitle:false,
+        IsFixHref : false 
     },
     {
         site:"it", //51cto
         url: ["https://www.51cto.com/"],
-        dv:'div.home-left-list li',
-        selector : "x.children[1].children[0].textContent + \"|||\" + x.children[1].children[0].href + \"|||\" + x.children[1].children[2].children[0].textContent ",
-        IsShow : true
+        each:'[class="article-ir articleItem"]', 
+        regHref:'<div class="article-irl-ct"(\\s|.)*?</div>', 
+        regDate:"(\\d{4}(-|/)\\d{1,2}(-|/)\\d{1,2}\\s\\d{2}:\\d{2}:\\d{2})", //2022-07-08 09:41:20
+        IsShow : true,
+        IsCuteTitle:false,
+        IsFixHref : false 
     },
-    {   
+    {
         site:"myzaker.com", 
-        url: ["https://www.myzaker.com/channel/10045","https://www.myzaker.com/"],
-        dv:'#contentList div.article-content',
-        selector : "x.children[0].textContent + \"|||\" + x.children[0].href + \"|||\" + x.children[1].children[1].textContent",
-        IsShow : true
-    },
-    {   
-        site:"myzaker精读", 
-        url: ["https://www.myzaker.com/?pos=selected_article"],
-        dv:'#contentList div.article-content',
-        selector : "x.children[0].textContent + \"|||\" + x.children[0].href + \"|||\" + x.children[1].children[1].textContent",
-        IsShow : true
-    },
-    {   
-        site:"stcn.com", 
-        url: ["https://stock.stcn.com/index.html","https://stock.stcn.com/index_[1-3].html"],
-        dv:"ul.news_list li",
-        selector: "x.children[0].children[0].textContent + \"|||\" + x.children[0].children[0].href + \"|||\" + x.children[1].textContent",
-        IsShow : true
-    },
-    {   
-        site:"博客", 
-        url: ["http://caifuhao.eastmoney.com/cfh/240651"],
-        dv:'ul.media_list li.item',
-        selector : "x.children[0].children[0].textContent + \"|||\" + x.children[0].children[0].href + \"|||\" + x.children[1].children[1].children[1].children[0].textContent",
-        IsShow : true
+        url: ["https://www.myzaker.com/"],
+        each:'[class="article-wrap"]', 
+        regHref:'<div class="article-content"(\\s|.)*?</div>', 
+        regDate:"(.(分钟前|小时前|天前))|(\\d{4}(-|/)\\d{1,2}(-|/)\\d{1,2})|(\\d{1,2}月(\s|.)*\\d{4})", 
+        IsShow : true,
+        IsCuteTitle:true ,
+        IsFixHref : false 
     },
     {
-        site:"博客", //招财猫
-        url: ["http://caifuhao.eastmoney.com/cfh/100043","http://caifuhao.eastmoney.com/cfh/123715"],
-        dv:'ul.media_list li.item',
-        selector : "x.children[0].children[0].textContent + \"|||\" + x.children[0].children[0].href + \"|||\" + x.children[1].textContent",
-        IsShow : true
+        site:"stcn", 
+        url: ["https://stock.stcn.com/index.html"],
+        each:'#idData li', 
+        regHref:'<p class="tit"(\\s|.)*?</p>', 
+        regDate:"(.(分钟前|小时前|天前))|(\\d{4}(-|/)\\d{1,2}(-|/)\\d{1,2})|(\\d{1,2}月(\s|.)*\\d{4})", 
+        IsShow : true,
+        IsCuteTitle:false ,
+        IsFixHref : false 
     },
     {
-        site:"hack", //
-        url: ["https://www.webshell.cc/"],
-        dv:'article.post:not(.whisper)',
-        selector : "x.children[0].children[0].textContent + \"|||\" + x.children[0].children[0].href + \"|||\" + x.children[1].children[0].textContent",
-        IsShow : true
+        site:"stcn", 
+        url: ["https://kuaixun.stcn.com/"],
+        each:'#news_list2 li', 
+        regHref:'', //如果为空, 则添加外层的div 
+        regDate:"(.(分钟前|小时前|天前))|(\\d{4}(-|/)\\d{1,2}(-|/)\\d{1,2})|(\\d{1,2}月(\s|.)*\\d{4})", 
+        IsShow : true,
+        IsCuteTitle:false,
+        IsFixHref : true 
     }
 ];
+
+// urls = [
+//     {
+//         site:"stcn", 
+//         url: ["https://kuaixun.stcn.com/"],
+//         each:'#news_list2 li', 
+//         regHref:'', //如果为空, 则添加外层的div 
+//         regDate:"(.(分钟前|小时前|天前))|(\\d{4}(-|/)\\d{1,2}(-|/)\\d{1,2})|(\\d{1,2}月(\s|.)*\\d{4})", 
+//         IsShow : true,
+//         IsCuteTitle:false 
+//     }
+// ];
 
 
 
@@ -94,54 +108,49 @@ var eCurl = async(url)=>
     return rt;
 }
 // 2 对html处理返回jons
-var getHref = async(html,obj_site) => {
+var getSqliteData = async(html,obj_site) => {
     objHref = {};
     rt = [];
     const $ = cheerio.load(html)
-    // const $ = cheerio.load('<h2 class="title">Hello world</h2>')
-    // let str= $("[class='normal-item normal-item-ie col-lg-4 col-md-4 col-sm-4 col-xs-12']");
-    let str= $("[class='normal-item col-lg-3 col-md-3 col-sm-4 col-xs-6']");
-    
-    // jquery循环
+    let str= $(obj_site.each);
+logger.debug(obj_site.url, new Date()); 
     $(str).each(function(i, item){    
-        // var title = $(this).children(1).children(1).children(0).text()
-        var html = $(this).html();
-        // var title = $(this).children().find('.title').text();
-        // var href = $(this).children().find('.title').children().attr('href');
-        // var title = $(this).children().attr('href');
-        // var text = $(this).children(0).text();
-        var title_str =  getRegStr(html,/<span class="title">(\s|.)*?<\/span>/); //title
+        let html = $(this).html();
+        let title_str =  getRegStr(html,obj_site.regHref); //title
+        let tm_title =  $(title_str).find('a').text(); //作废: let tm_title = $(title_str).text().trim();
+        let html_date = getRegStr(html, obj_site.regDate);
+        let tm_href =  $(title_str).find('a').attr('href');
+        if(obj_site.IsCuteTitle)tm_title = tm_title.substr(0,tm_title.indexOf('\n'));
+
         objHref.site = obj_site.site;
-        objHref.href = $(title_str).find('a').attr('href');
-        objHref.title = $(title_str).text().trim();
-        objHref.date =  getRegStr(html,/((.(分钟前|小时前|天前))|(\d{4}-\d{1,2}-\d{1,2}))/); 
-console.log(JSON.stringify(objHref)+IndexA())
+        objHref.href = FixHref(tm_href,obj_site);
+        objHref.title = tm_title;
+        objHref.date_source = html_date;
+        objHref.date = lib.handTime(html_date)  ; 
+
+logger.debug(JSON.stringify(objHref));
         rt.push(objHref)
         // return false;
     });
     return str;
-
-    // var 正则获取href ;
-    // let arr= [];
-    // for (const B of html) {
-    //     title.site = A.site ; title.dateTime = lib.handTime(title.dateTime);
-    //     arr.push(title);
-    // }
-    // return arr;
 }
 var getRegStr =(html,reg) => {
-    var rt2 = html.match(reg)[0];
+    if(reg=="") return "<div>"+ html + "</div>";
+    var re = new RegExp(reg)
+    if(html.match(re) == null) return "";
+    var rt2 = html.match(re)[0];
     return rt2;
 }
 
 //3.A 循环各个站点,把[1-2]变成数组元素
 var RebuildPageRS = (obj) => {
     var rt = [];
-    for (const A of obj) {                      //分解urls对象
+    for (const A of obj) {    
+        if(!A.IsShow) continue;                  //分解urls对象
         for (const B of A.url) {                //分解url地址数组
             var PageNOs = lib.Exp2Array(B);     //分解表达式test_[1-3]_html,成数组'
             for (const el_url of PageNOs) {
-                var New = { site: A.site , url: el_url, dv: A.dv , selector : A.selector , IsShow : A.IsShow };  
+                var New = { site: A.site , url: el_url, regHref: A.regHref , regDate : A.regDate , IsShow : A.IsShow ,each:A.each,IsCuteTitle : A.IsCuteTitle,IsFixHref : A.IsFixHref};  
                 rt.push(New); 
             }   
         }
@@ -152,18 +161,22 @@ var RebuildPageRS = (obj) => {
 var ForUrlPage = async(obj) => {
     var site_Arr = RebuildPageRS(obj) //重组urls数组
     for (const obj_site of site_Arr) {        //循环页面
-        var rt = await eCurl(obj_site.url); //取得页面title的html集合
-        // console.log(rt)
-        let obj = getHref(rt,obj_site)
-        break;
+        var PageCodeOrJson = await eCurl(obj_site.url); //取得页面title的html集合
+        let obj = getSqliteData(PageCodeOrJson,obj_site)
     }   
-    // return TitleRS;
 }
-
-
+//3.c 修正href的相对链接 './aa.html'=>'http://xxx.com/aa.html'
+var FixHref = (href,obj_site) => {
+    var rt = href;
+    if(obj_site.IsFixHref){
+        rt = obj_site.url + href.replace('./','');
+    }
+    return rt;
+}
 
 //5 每3个小时定时轮询
 (async()=>{
+    fs.unlinkSync("./log/default.log")
     ForUrlPage(urls);
     // let rule = new schedule.RecurrenceRule();       
     // // rule.minute = 30;rule.second = 0;   
