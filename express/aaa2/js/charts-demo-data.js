8e0900c6aae1e0aa251a82ca68429066b1ea2138
chusiping <chusiping@163.com>
2022-08-28 13:02:17 +0800

    2022-8-28 13:01 分时k线修改为promise方式

--- express/aaa2/js/charts-demo-data.js
+++ express/aaa2/js/charts-demo-data.js
@@ -1,38 +1,49 @@
 //代码来源： https://gitee.com/ls_web/echarts-demo
-var mdata = { "data": [] }
-function getmData(callback) { //分时图
-    $.ajax({
-        // url: "http://data.gtimg.cn/flashdata/hushen/minute/sh601066.js?maxage=110&0.28163905744440854",
-        url: "http://127.0.0.1:3004/getMinite_KLine?code=601066",  //分时数据
-        dataType: "json",
-        cache: "false",
-        type: "GET",
-        success: function (rt) {
-            let _arr = [];
-            for (var i = 0; i < rt.data.length; i++) {
-                let _rt = [rt.data[i].m, rt.data[i].p, rt.data[i].v,];
-                _arr.push(_rt)
+function getmData(_code) { //分时图 改为promise写法
+    return new Promise((resolve, reject) => {
+        var mdata = { "data": [] }
+        $.ajax({
+            // url: "http://data.gtimg.cn/flashdata/hushen/minute/sh601066.js?maxage=110&0.28163905744440854",
+            url: "http://127.0.0.1:3004/getMinite_KLine?code="+_code,  //分时数据
+            dataType: "json",
+            cache: "false",
+            type: "GET",
+            success: function (rt) {
+                let _arr = [];
+                for (var i = 0; i < rt.data.length; i++) {
+                    let _rt = [rt.data[i].m, rt.data[i].p, rt.data[i].v,];
+                    _arr.push(_rt)
+                }
+                mdata.data = _arr
+                resolve(mdata)
+            },
+            error: function () {
+                reject("wrong");
             }
-            mdata.data = _arr
-            callback(mdata)
-        },
-        error: function () {
-            alert("wrong");
-        }
+        });
     });
 }
-function getInfo(callback) { //获取股票昨天的收盘价格
-    $.ajax({
-        url: "http://127.0.0.1:3004/test?code=601066",
-        dataType: "script",
-        cache: "false",
-        type: "GET",
-        success: function () {
-            var info = eval('hq_str_sh601066').split(",")
-            callback(info)
-        },
-        error: function () {
-            alert("查询信息失败")
-        }
-    })
+let GetSixCode = function (code){
+    let mk = "sz";
+    if(code.substring(0,1) == "6") mk = "sh";
+    if(code.substring(0,1) == "5") mk = "sh";
+    return mk+code;
+};
+function getInfo(_code) { //获取股票昨天的收盘价格
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: "http://127.0.0.1:3004/test?code="+_code,
+            dataType: "script",
+            cache: "false",
+            type: "GET",
+            success: function () {
+                t_code = GetSixCode(_code);
+                var info = eval('hq_str_'+t_code).split(",")
+                resolve(info)
+            },
+            error: function () {
+                reject("查询信息失败")
+            }
+        })
+    });
 }
