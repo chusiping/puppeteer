81b6c4da673345e04838b282a7aa8eb80224dd1b
chusiping <chusiping@163.com>
2023-07-10 13:54:15 +0800

    2023-7-10 13:54 yh

--- OA的sqlserver查询/html.js
+++ OA的sqlserver查询/html.js
@@ -1,4 +1,5 @@
 // 参考 2023-7-7 https://blog.csdn.net/weixin_44726970/article/details/121928476
+// 代码逻辑说明见底部
 var request = require('request');
 const express = require('express');
 const app = express();
@@ -57,15 +58,16 @@ function RowToJson2(result,Column英文名) {
 };
 
 function CombinSql(str) {
-    return "select requestid from workflow_requestbase where requestname like '"+ str +"%'";
+    const startsWithLetterRegex = /^[a-zA-Z]/;
+    if(!startsWithLetterRegex.test(str)){
+        return "select requestid from workflow_requestbase where requestname like '"+ str +"%'";
+    }else{
+        return "select requestid from workflow_requestbase where requestmark = '"+ str +"'";
+    }
 };
 
-
-var sql_1=` DECLARE @requestID VARCHAR(20)
-            select @requestID=requestid from workflow_requestbase where requestname like '采购立项审批流程(职能部门专用)-范秉淇-2023-05-29%'
-
-            DECLARE @billID VARCHAR(20)
-            select @billID=billformid from workflow_form where requestid=@requestID
+var sql_1a=`DECLARE @billID VARCHAR(20)
+            select @billID=billformid from workflow_form where requestid=_requestID_
 
             DECLARE @tableName VARCHAR(100)
             SELECT @tableName=tablename from workflow_bill where id=@billID
@@ -73,7 +75,7 @@ var sql_1=` DECLARE @requestID VARCHAR(20)
             DECLARE @sqlstr NVARCHAR(MAX)
             set @sqlstr='select b.billformid,a.* from ' + @tableName + ' a left join workflow_form b
             on a.requestId=b.requestid
-            where a.requestId=' + @requestID
+            where a.requestId=_requestID_'
             exec(@sqlstr) `;
 
 var billID = "0";
@@ -81,46 +83,38 @@ var sql_2=` select b.billid, b.fieldname, b.fieldlabel,l.labelname,fielddbtype
             from workflow_billfield b
             left JOIN HtmlLabelInfo l 
             on b.fieldlabel=l.indexid and l.languageid=7
-            where billid=`;
-
-function CombinSql2(str) {
-    return sql_2 + str;
-};
+            where billid=_billid_`;
 
             
 // sql_1='SELECT top 3 id,lastname,mobile,created from hrmresource';
 
 app.get("/",(req,res)=>{
 
+    let flowName = req.query.flowname;
+
+    if (!flowName) {
+        return res.status(400).send('缺少必需的参数flowName！');
+    }
+
     let rs;
-    var str = CombinSql("采购立项审批流程(职能部门专用)-范秉淇-2023-05-29")
+    var str = CombinSql(flowName);
+    // var str = CombinSql("采购立项审批流程(职能部门专用)-范秉淇-2023-05-29")
+
     MyQuery(str).then(result => {       //解释：返回result = requestID
-        return MyQuery(sql_1);          //解释：返回审批表单的fieldlabe      
+        let requestID = result.recordset[0]["requestid"];
+        let sql = sql_1a.replace(/_requestID_/g,requestID)
+        return MyQuery(sql);            //解释：返回审批表单的fieldlabe      
     })
     .then(result => { 
         rs=result;
         let id=result.recordset[0]["billformid"];
-        let sql =  CombinSql2(id)
+        let sql = sql_2.replace("_billid_",id)  //解释：返回fieldlabel对应的中文
         return MyQuery(sql);
     })
     .then(result => { 
-        let rt = SwitchName(rs,result)
+        let rt = SwitchName(rs,result)          //解释：替换成中文
         res.json(rt);  
     })
-
-
-
-    // var Rows ;
-    // MyQuery(sql_1).then(result => { 
-    //     // res.json(RowToJson(result) )
-    //     Rows=result;
-    //     return MyQuery(sql_2 + result.recordset[0]["billformid"]);
-    // })
-    // .then(result => { 
-    //     res.json(SwitchName(Rows,result) )
-    // })
-
-
 
 });
 
@@ -128,3 +122,15 @@ app.listen(3000)
 
 
 
+/* ****** 代码逻辑 ***********
+
+1. CombinSql 根据条件查出requestID
+2. requestID 传给 sql_1a 
+3. sql_1a 返回审批表单的fieldlabel  
+4. sql_2  返回fieldlabel对应的中文
+5. SwitchName 切换sql里字段的英文label为中文label
+6. 测试
+    127.0.0.1:3000/?flowname=采购立项审批流程(职能部门专用)-范秉淇-2023-05-29
+    127.0.0.1:3000/?flowname=GCXMYJS2023040008
+
+*/
