3dc5e560ac237f64e6b07afc6afb551f3e4f106a
chusiping <chusiping@163.com>
2023-08-04 15:33:35 +0800

    2023-8-4 15:33 yh

--- chatGpt_mongodb/chatGPT_mongodb_express.js
+++ chatGpt_mongodb/chatGPT_mongodb_express.js
@@ -4,17 +4,16 @@ const { insertData, getClientIp,getCurrentDateTime } = require('./chatGPT_mongod
 
 const app = express();
 app.use(bodyParser.json());
-app.post('/insert', function (req, res) {
-  const data = req.body;
-  data.addTime = getCurrentDateTime();
-  data.ip = getClientIp(req);
-  insertData({},data) // 重载方法 insertData(data,url, dbName, collectionName ) 
-    .then((result) => {
-      console.log(result);
-      res.send(result);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+app.post('/insert', async function (req, res) {
+  try {
+    const data = req.body;
+    data.addTime = getCurrentDateTime();
+    data.ip = getClientIp(req);
+    const result = await insertData({}, data);
+    console.log(result);
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+  }
 });
 app.use('', express.static('./')).listen(3000);
