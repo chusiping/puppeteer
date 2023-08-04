3135692488a629d50dc39f536e2415f113e0ab38
chusiping <chusiping@163.com>
2023-08-04 16:21:40 +0800

    2023-8-4 16:21 add mongodb的删除方法

--- chatGpt_mongodb/chatGPT_mongodbUtils.js
+++ chatGpt_mongodb/chatGPT_mongodbUtils.js
@@ -1,5 +1,28 @@
 const MongoClient = require('mongodb').MongoClient;
 
+async function deleteData(dbcoon, filter) {
+  if (!dbcoon.url) dbcoon.url = 'mongodb://myuser:mypassword@redis.qy:27017/mydatabase';
+  if (!dbcoon.dbName) dbcoon.dbName = 'mydatabase';
+  if (!dbcoon.collectionName) dbcoon.collectionName = 'mycollection';
+  try {
+    const client = await MongoClient.connect(dbcoon.url, { useUnifiedTopology: true });
+    const db = client.db(dbcoon.dbName);
+    const collection = db.collection(dbcoon.collectionName);
+    const count = await collection.countDocuments(filter);
+    if (count === 0) {
+      client.close();
+      return { success: false, message: '没有数据需要删除' };
+    }
+    const result = await collection.deleteMany(filter);
+    client.close();
+    return { success: true, message: '删除成功', deletedCount: result.deletedCount };
+  } catch (error) {
+    return { success: false, message: '删除失败', error: error };
+  }
+}
+
+
+
 async function insertData(dbcoon, data) {
   if (!dbcoon.url) dbcoon.url = 'mongodb://myuser:mypassword@redis.qy:27017/mydatabase';
   if (!dbcoon.dbName) dbcoon.dbName = 'mydatabase';
@@ -11,7 +34,7 @@ async function insertData(dbcoon, data) {
     const result = await collection.insertOne(data);
     // console.log('操作成功:', result.insertedId);
     client.close();
-    return { success: true, message: 'inserted successfully', insertedId: result.insertedId };
+    return { success: true, message: '添加成功', insertedId: result.insertedId };
   } catch (error) {
     // console.error('Failed to insert document:', error);
     return { success: false, message: '操作失败', error: error };
@@ -42,7 +65,7 @@ function getCurrentDateTime() {
 }
 
 
-module.exports = { insertData, getClientIp, getCurrentDateTime };
+module.exports = { deleteData,insertData, getClientIp, getCurrentDateTime };
 
 
 
--- chatGpt_mongodb/chatGPT_mongodb_express.js
+++ chatGpt_mongodb/chatGPT_mongodb_express.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { insertData, getClientIp,getCurrentDateTime } = require('./chatGPT_mongodbUtils');
+const { deleteData,insertData, getClientIp,getCurrentDateTime } = require('./chatGPT_mongodbUtils');
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,4 +16,18 @@ app.post('/insert', async function (req, res) {
     console.error(error);
   }
 });
+
+// 删除数据
+app.post('/delete', async function (req, res) {
+  try {
+    const filter = req.body;
+    const result = await deleteData({}, filter);
+    console.log(result);
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+
 app.use('', express.static('./')).listen(3000);
