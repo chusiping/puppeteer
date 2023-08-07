cbc9e908f20e6b34302d582cf6160cb139f8da91
chusiping <chusiping@163.com>
2023-08-07 18:12:34 +0800

    2023-8-7 18:12 增删改查合并到一个html

--- chatGpt_mongodb/static/script.js
+++ chatGpt_mongodb/static/script.js
@@ -23,9 +23,46 @@ $(function () {
                 }
             });
         });
+       
+    }
+    function findData(buttonID,FormID,api,tableName) {
+        $(buttonID).on('click', function (e) {
+            e.preventDefault();
+            const formData = $(FormID).serializeArray().reduce((obj, item) => {
+                let v = item.value.trim()
+                if(v) obj[item.name] = v;
+                return obj;
+            }, {});
+
+            $.ajax({
+                type: 'POST',
+                url: api ,
+                data: JSON.stringify(formData),
+                contentType: 'application/json',
+                success: function (res) {
+                    displayData(tableName,res.data);
+                },
+                error: function (error) {
+                    console.log(error);
+                }
+            });
+        });
+    }
+    function displayData(tableName,data) {
+        var tableBody = $(tableName + " tbody");
+        tableBody.empty();
+        $.each(data, function (index, obj) {
+            var row = $("<tr>");
+            row.append($("<td>").text(index));
+            for (var key in obj) {
+                row.append($("<td>").text(key + ": " + obj[key]));
+            }
+            tableBody.append(row);
+        });
     }
     submitForm('#submitBtn','#myForm','http://127.0.0.1:3000/insert');
     submitForm('#deleteBtn','#myForm','http://127.0.0.1:3000/delete');
     submitForm('#editBtn', '#myForm', 'http://127.0.0.1:3000/update');
+    findData('#findBtn', '#myForm', 'http://127.0.0.1:3000/find','#dataTable');
 });
 
