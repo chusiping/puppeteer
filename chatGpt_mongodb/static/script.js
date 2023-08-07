5c16c9cede5a5944035662077201dc81858167a1
chusiping <chusiping@163.com>
2023-08-07 17:23:24 +0800

    2023-8-7 17:22  MongoDB的修改js解耦

--- chatGpt_mongodb/static/script.js
+++ chatGpt_mongodb/static/script.js
@@ -1,20 +1,20 @@
 $(function () {
-    function submitForm(buttonID,FormID) {
+    function submitForm(buttonID,FormID,api) {
         $(buttonID).on('click', function (e) {
             e.preventDefault();
             const formData = $(FormID).serializeArray().reduce((obj, item) => {
-                obj[item.name] = item.value.trim();
+                let v = item.value.trim()
+                if(v) obj[item.name] = v;
                 return obj;
             }, {});
 
             $.ajax({
                 type: 'POST',
-                url: 'http://127.0.0.1:3000/insert',
+                url: api ,
                 data: JSON.stringify(formData),
                 contentType: 'application/json',
                 success: function (res) {
-                    const rt = res.success ? "添加成功" : "操作失败"
-                    const successMessage = $('<div>').text(rt).addClass('success-message');
+                    const successMessage = $('<div>').text(res.message).addClass('success-message');
                     $('body').prepend(successMessage);
                     setTimeout(() => successMessage.remove(), 2000);
                 },
@@ -24,6 +24,8 @@ $(function () {
             });
         });
     }
-    submitForm('#submitBtn','#myForm');
+    submitForm('#submitBtn','#myForm','http://127.0.0.1:3000/insert');
+    submitForm('#deleteBtn','#myForm','http://127.0.0.1:3000/delete');
+    submitForm('#editBtn', '#myForm', 'http://127.0.0.1:3000/update');
 });
 
